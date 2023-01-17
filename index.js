e438a218afcdfc46313f3b18ad09019f7494fffe
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-01-17 18:04:43 -0300

    chore: send new test

--- index.js
+++ index.js
@@ -42,3 +42,53 @@ axios.request(options).then(response => {
   console.error(error);
 });
 
+// or another way inside admin
+const body = {
+  sort: [{_id: {order: 'desc'}}],
+  aggs: {
+    'brands.name': {terms: {field: 'brands.name'}},
+    'categories.name': {terms: {field: 'categories.name'}},
+    status: {terms: {field: 'status'}},
+    min_price: {min: {field: 'price'}},
+    max_price: {max: {field: 'price'}},
+    avg_price: {avg: {field: 'price'}}
+  },
+  size: 3500,
+  from: 0
+}
+window.callSearchApi('items.json', 'POST', (err, json) => {
+  if (!err) {
+    const { hits } = json.hits
+    const allProducts = []
+    let item
+    hits.forEach((hit, index) => {
+      item = hit._source
+      if (item) {
+        const categories = (item) => {
+          let result = ''
+          let categories = item.categories
+          if (Array.isArray(categories) && (categories.length > 0)) {
+            categories.forEach(cat => {
+              return result += `,${cat.slug}`
+            })
+          } 
+          return result
+        }
+          let product = {
+              "_id": String(hit._id),
+              "sku": String(item.sku),
+              "name": String(item.name),
+              "brands": item.brands && item.brands[0] && item.brands[0].name ? String(item.brands[0].name) : undefined,
+              "slug": String(item.slug),
+              "slug_categories": categories(item),
+              "price": Number(item.price),
+              "ad_relevance": Number(item.ad_relevance)
+              //"quantity": Number(item.quantity)
+              }
+              allProducts.push(JSON.stringify(product))
+      }
+    })
+    console.dir(allProducts, { maxArrayLength: null }) 
+  }
+}, body)
+
